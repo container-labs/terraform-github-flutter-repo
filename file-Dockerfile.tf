796be96f0ab35c61b12612169c1ae264fcb47115
TF Robot <robot@containerlabs.com>
2021-02-13 18:07:09 +0000

    Committing terraform fmt changes

--- file-Dockerfile.tf
+++ file-Dockerfile.tf
@@ -2,13 +2,13 @@ locals {
   dockerfile = templatefile(
     "${path.module}/files/Dockerfile.tpl",
     {
-    #   directory            = var.dependabot_directory
+      #   directory            = var.dependabot_directory
     }
   )
 }
 
 resource "github_repository_file" "dockerfile" {
-#   count      = var.dependabot_enabled ? 1 : 0
+  #   count      = var.dependabot_enabled ? 1 : 0
   repository = var.repository
   branch     = "main"
   file       = ".devcontainer/Dockerfile"
